b6cc912db76f5147af4d02c526ab88f8207159ff
Mohamed Rachid <mohamednrachid@gmail.com>
2022-09-12 18:21:25 -0400

    Added RecyclerView to display content

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listOfTasks.add("Do laundry")
+        listOfTasks.add("Go to the bank")
+
+        // Lookup RecyclerView in the layout
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        // Create adapter passing in sample user data
+        val adapter = TaskItemAdapter(listOfTasks)
+
+        // Attach adapter to RecyclerView to populate items
+        recyclerView.adapter = adapter
+
+        // Set layout manager to position items
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
